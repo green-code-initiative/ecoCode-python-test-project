8263b6e0f9d7822930979e17baf266a4a21c0cbd
David DE CARVALHO <dedece35@gmail.com>
2023-06-14 12:46:09 +0200

    BREIZHCAMP23 - correction use case - BIS

--- src/starWarsLegion_trainings.py
+++ src/starWarsLegion_trainings.py
@@ -1,8 +1,14 @@
+weapons_list = ['saber_green', 'saber_blue', 'saber_red']
+
 ##########
 # NON COMPLIANT for "list comprehension"
 ##########
 
-def non_compliant_list_training():
+def non_compliant_training_1():
+    for current_weapon in [weapon for weapon in weapons_list]: # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
+        print(current_weapon)
+
+def non_compliant_training_2():
     for current_weapon in [weapon_number for weapon_number in range(1000)]: # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
         print(current_weapon)
 
@@ -11,14 +17,22 @@ def non_compliant_list_training():
 # COMPLIANT for "list comprehension"
 ##########
 
-def compliant_basic_training():
-    for current_weapon in ('saber_green', 'saber_blue', 'saber_red'):
+def compliant_training_1():
+    for current_weapon in weapons_list:
         print(current_weapon)
 
-def compliant_basic2_training():
+def compliant_training_2():
     for current_weapon in range(10):
         print(current_weapon)
 
-def compliant_list_training():
+def compliant_training_3():
+    for current_weapon in ('saber_green', 'saber_blue', 'saber_red'):
+        print(current_weapon)
+
+def compliant_training_4():
+    for current_weapon in ['saber_purple', 'saber_pink', 'saber_white']:
+        print(current_weapon)
+
+def compliant_training_5():
     for current_weapon in (weapon_number for weapon_number in range(1000)):
         print(current_weapon)
