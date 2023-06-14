ba5013d79bfbf46f67bf409a7eddb249ab6e8a69
David DE CARVALHO <dedece35@gmail.com>
2023-06-14 11:27:35 +0200

    BREIZHCAMP23 - Add test file

--- src/starWarsLegion_trainings.py
+++ src/starWarsLegion_trainings.py
@@ -1,31 +1,28 @@
+##########
+# NON COMPLIANT
+##########
+
+def compliant_basic_training():
+    for current_weapon in ["saber_green", "saber_blue", "saber_red"]: # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
+        print(current_weapon)
+
 def non_compliant_list_training():
     for current_weapon in [weapon_number for weapon_number in range(1000)]: # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
         print(current_weapon)
 
-def non_compliant_enumerate_training():
-    for idx, current_weapon in enumerate([weapon_number for weapon_number in range(1000)]): # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
-        print(current_weapon)
 
-def non_compliant_zip_training():
-    for current_weapon, current_weapon2 in zip([weapon_number for weapon_number in range(1000)], [weapon2_number for weapon2_number in range(1000)]): # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}} {{Use generator comprehension instead of list comprehension in for loop declaration}}
-        print(current_weapon)
+##########
+# COMPLIANT
+##########
 
 def compliant_basic_training():
-    for current_weapon in range(10):
+    for current_weapon in ("saber_green", "saber_blue", "saber_red"):
         print(current_weapon)
 
-def compliant_filter_training():
-    for current_weapon in filter(lambda weapon_number: weapon_number > 2, range(100)):
+def compliant_basic2_training():
+    for current_weapon in range(10):
         print(current_weapon)
 
 def compliant_list_training():
     for current_weapon in (weapon_number for weapon_number in range(1000)):
-        print(current_weapon)
-
-def compliant_enumerate_training():
-    for idx, current_weapon in enumerate(range(1000)):
-        print(current_weapon)
-
-def compliant_zip_training():
-    for current_weapon, current_weapon2 in zip((weapon_number for weapon_number in range(3)), ["saber_green", "saber_blue", "saber_red"]):
         print(current_weapon)
