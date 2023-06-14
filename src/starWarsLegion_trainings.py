d869822c5f477260185e77f3f95c66fd0fcdc863
David DE CARVALHO <dedece35@gmail.com>
2023-06-14 12:38:16 +0200

    BREIZHCAMP23 - correction use case

--- src/starWarsLegion_trainings.py
+++ src/starWarsLegion_trainings.py
@@ -2,10 +2,6 @@
 # NON COMPLIANT for "list comprehension"
 ##########
 
-def compliant_basic_training():
-    for current_weapon in ['saber_green', 'saber_blue', 'saber_red']: # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
-        print(current_weapon)
-
 def non_compliant_list_training():
     for current_weapon in [weapon_number for weapon_number in range(1000)]: # Noncompliant {{Use generator comprehension instead of list comprehension in for loop declaration}}
         print(current_weapon)
